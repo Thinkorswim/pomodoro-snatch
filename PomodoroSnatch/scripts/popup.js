838db311bc5029743f8cd9f40b9780b3c2e9a110
Martin Georgiev <martingeorgiev1995@gmail.com>
2017-10-04 20:21:52 +0100

    Basic version
    - Counter 25->5
    - Counter design
    - Initial logo design

--- PomodoroSnatch/scripts/popup.js
+++ PomodoroSnatch/scripts/popup.js
@@ -1,89 +1,60 @@
 $(function(){
 
-  // On Load
-  listBlockedWebsites();
+  $("#controllButton").click(function(){
 
-
-  chrome.storage.sync.get('date', function(data){
-    var currentDate = getDateFormat(new Date());
-
-    if(data.date && currentDate != data.date){
-        resetDayTimes();
+    if($("#controllButton").hasClass("startPomodoro")){
+      $("#controllButton").removeClass("startPomodoro").addClass( "voidPomodoro" );
+    }else{
+      // TODO reset everything
+      $("#controllButton").removeClass("voidPomodoro").addClass( "startPomodoro" );
     }
+
+    chrome.runtime.sendMessage({
+      type: "startPomodoro",
+    });
   });
+
+  chrome.runtime.sendMessage({
+    type: "getPomodoroTime",
+  });
+
+  let wholeTime = 0;
+  let progressBar = document.querySelector('.e-c-progress');
+  let indicator = document.getElementById('e-indicator');
+  let pointer = document.getElementById('e-pointer');
+  let length = Math.PI * 2 * 100;
+  const displayOutput = document.querySelector('.display-remain-time')
+  const pauseBtn = document.getElementById('pause');
+  const setterBtns = document.querySelectorAll('button[data-setter]');
 
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    $("#blocked" + request.listId + ' td:eq("1")').html(request.time);
-    // $("#blocked" + request.listId).css('background-color', '#2980b9');
-    // $("#blocked" + request.listId).css('color', 'white');
-  });
+    if(request.type == "updateTime"){
+      wholeTime = request.timeTotal;
+      displayTimeLeft(request.time);
 
-
-
-  $(".popupButton, #cogButton").click(function(){
-    chrome.runtime.openOptionsPage();
-  });
-
-
-  function resetDayTimes(){
-      var currentDate = getDateFormat(new Date());
-      chrome.storage.sync.set({'date': currentDate});
-
-      chrome.storage.sync.get('blockList', function(data){
-          if(data.blockList && data.blockList.length){
-            var blockList = data.blockList;
-            for(i in blockList){
-              blockList[i].timeDay = 0;
-            }
-
-            chrome.storage.sync.set({'blockList': blockList});
-          }
-
-          listBlockedWebsites();
-      });
-  }
-
-
-  function listBlockedWebsites(){
-    chrome.storage.sync.get('blockList', function(data){
-        if(data.blockList && data.blockList.length){
-          var blockList = data.blockList;
-          $("#popupList table").html('');
-
-          for (i in blockList) {
-              var timeLeft = getMinutesAndSeconds(blockList[i].timeDay, blockList[i].timeTotal);
-              var minutes = Math.floor((blockList[i].timeTotal-blockList[i].timeDay) / 60);
-              var seconds = (blockList[i].timeTotal-blockList[i].timeDay) % 60;
-
-              if(minutes < 0 || (minutes == 0 && seconds == 0)){
-                timeLeft = "None";
-              }
-
-              var popupRow = '<tr id="blocked' + i + '">';
-              popupRow += '<td class="pBlocked">' + blockList[i].url + '</td>';
-              popupRow += '<td class="pTime">' + timeLeft + '</td>';
-              popupRow += "</tr>"
-
-              $("#popupList table").append(popupRow);
-          }
-        }else{
-          var popupRow = '<td class="noBlocked"> No blocked websites :( </td>';
-          $("#popupList table").append(popupRow);
-        }
-    });
-  }
-
-  function getMinutesAndSeconds(day, total){
-    var minutes = (Math.floor((total-day) / 60)).toString()
-    var seconds = ((total - day) % 60);
-    if(seconds < 10){
-      seconds = '0' + seconds.toString();
+      if(request.time != request.timeTotal){
+        $("#controllButton").removeClass("startPomodoro").addClass( "voidPomodoro" );
+      }
+    }else if(request.type == "endPomodoro"){
+      $("#controllButton").removeClass("voidPomodoro").addClass( "startPomodoro" );
     }
-    return minutes + ":" + seconds;
-  }
+  });
 
-  function getDateFormat(date){
-    return date.getDay().toString() + "/" + date.getMonth().toString() + "/" + date.getFullYear().toString();
+  progressBar.style.strokeDasharray = length;
+
+  function update(value, timePercent) {
+    var offset = - length - length * value / (timePercent);
+    progressBar.style.strokeDashoffset = offset;
+    pointer.style.transform = `rotate(${360 * value / (timePercent)}deg)`;
+  };
+
+
+  function displayTimeLeft (timeLeft){
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let displayString = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    displayOutput.textContent = displayString;
+    update(timeLeft, wholeTime);
   }
 
 });
